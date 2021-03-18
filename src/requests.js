cb79ba8134e3f4fe03225434b4d0dc86bb0d46da
github username <dirtyassweed@icloud.com>
2021-03-18 11:15:44 -0400

    configures axios for hitting TMDB API

---
+++ src/requests.js
@@ -0,0 +1,18 @@
+const API_KEY = 'd05a37524843d0668c5ec3d07c340133';
+
+export default {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchMystery: `/discover/movie?api_key=${API_KEY}&with_genres=9649`,
+    fetchSciFi: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+    fetchWestern: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+    fetchAnimation: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+    fetchTV: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+    
+};
+
+requests.fetchTrending
